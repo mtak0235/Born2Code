a3623ab2ee84aa288e451542d07881c23239d056
Min Kyeong - Tak <48946398+mtak0235@users.noreply.github.com>
2021-02-11 03:31:29 +0900

    Update ft_utils.h

--- printf/ft_utils.h
+++ printf/ft_utils.h
@@ -1,8 +1,8 @@
 #ifndef FT_UTILS_H
 # define FT_UTILS_H
 
-#include <string.h>
-#include <stdlib.h>
+# include <string.h>
+# include <stdlib.h>
 
 char	*ft_strdup(const char *str);
 size_t	ft_strlen(const char *s);
