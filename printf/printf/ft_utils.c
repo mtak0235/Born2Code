b28970ec16f6cf9b6aa3bfbb790839e8c165fc55
Min Kyeong - Tak <48946398+mtak0235@users.noreply.github.com>
2021-02-17 15:58:06 +0900

    Update ft_utils.c
    big deal

--- printf/printf/ft_utils.c
+++ printf/printf/ft_utils.c
@@ -20,6 +20,51 @@ char	*ft_strdup(const char *str)
 	return (new_str);
 }
 
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (fd < 0 || !s)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+static int	ft_itoa_len(long int n)
+{
+	int	len;
+
+	len = n <= 0 ? 1 : 0;
+	while (n)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char		*ft_itoa(int a)
+{
+	char		*ret;
+	int			sign;
+	int			len;
+	long int	n;
+
+	n = a;
+	sign = n < 0 ? -1 : 1;
+	len = ft_itoa_len(n);
+	if (!(ret = (char *)malloc(sizeof(char) * len + 1)))
+		return (0);
+	ret[len] = 0;
+	len--;
+	n *= sign;
+	while (len >= 0)
+	{
+		ret[len--] = 48 + n % 10;
+		n /= 10;
+	}
+	if (sign == -1)
+		ret[0] = '-';
+	return (ret);
+}
+
 size_t	ft_strlen(const char *s)
 {
 	size_t cnt;
@@ -40,6 +85,13 @@ int	ft_toupper(int c)
 	if ('a' <= c && c <= 'z')
 		return (c - 32);
 	return (c);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	if (fd < 0)
+		return ;
+	write(fd, &c, 1);
 }
 
 void init_flag(t_flag *flag)
@@ -66,15 +118,14 @@ void	fit_flag(t_flag *flag)
 		flag->zero = 0;
 }
 
-char *int2hex(unsigned long long num, int *cnt)
+char *int2hex(unsigned long long num, int pin)
 {
 	char *str;
 	char *hex;
 	int len;
 
-	hex = "0123456789abcdef";
+	hex = pin ? "0123456789ABCDEF" : "0123456789abcdef";
 	len = int2hex_len(num);
-	*cnt = len;
 	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
 		return (0);
 	str[len] = 0;
@@ -86,6 +137,27 @@ char *int2hex(unsigned long long num, int *cnt)
 	return (str);
 }
 
+char *ptr2hex(unsigned long long num)
+{
+	char *str;
+	char *hex;
+	int len;
+
+	hex = "0123456789abcdef";
+	len = int2hex_len(num) + 2;
+	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
+		return (0);
+	str[len] = 0;
+	while (--len >= 2)
+	{
+		str[len] = hex[num % 16];
+		num /= 16;
+	}
+	str[0] = '0';
+	str[1] = 'x';
+	return (str);
+
+}
 int int2hex_len(unsigned long long n)
 {
 	int len;
