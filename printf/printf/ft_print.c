54aaadf037e2dca5ae620588ff155e0b2dbdf055
Min Kyeong - Tak <48946398+mtak0235@users.noreply.github.com>
2021-02-17 15:55:16 +0900

    Update ft_print.c
    big deal

--- printf/printf/ft_print.c
+++ printf/printf/ft_print.c
@@ -1,190 +1,85 @@
 #include "ft_printf.h"
 #include "ft_utils.h"
 
-int print_char(t_flag *flag, va_list ap)
+static void print_flag(t_flag *flag, char *num, size_t size, int ost)
 {
-	int cnt;
-	char c;
-
-	cnt = 1;
-	c = (char)va_arg(ap, int);
-	if (flag->width < 1)
-		flag->width = 1;
-	if (flag->minus == 1)
-		write(1, &c, 1);
-	while (--(flag->width))
-	{
-		write(1, " ",1);
-		cnt++;
-	}
-	if (flag->minus == 0)
-		write(1, &c, 1);
-	return (cnt);
+        if (flag->indent)
+                ft_putchar_fd(' ', 1);
+        flag->width = size > flag->width ? size : flag->width;
+        if (flag->minus == 0)
+                while (size < flag->width)
+                {
+                        ft_putchar_fd(' ', 1);
+                        size++;
+                }
+        if (num[0] == '0' && num[1] == 'x')
+        {
+                ft_putstr_fd("0x", 1);
+                ost = 2;
+        }
+        while (flag->precision > 0 && flag->precision--)
+                ft_putchar_fd('0', 1);
+        ft_putstr_fd(num + ost, 1);
+        if (flag->minus == 1)
+                while (size < flag->width)
+                {
+                        size++;
+                        ft_putchar_fd(' ', 1);
+                }
 }
 
-int print_string(t_flag *flag, va_list ap)
+int                             print_hex(t_flag *flag, va_list ap, int pin)
 {
-	int cnt;
-	char *str;
-	int len;
-	int i;
-	int j;
-	
-	if(!(str = va_arg(ap, char *)))
-	{
-		write(1, "(null)", 6);
-		return (-1);
-	}
-	len = ft_strlen(str);
-	i = len < flag->precision || (flag->precision == -1) ? len : flag->precision;
-	i = i < 0 ? len : i;
-	cnt = flag->width < i ? i : flag->width;
-	j = (flag->width) - i;
-	while (j > 0 && !flag->minus)
-		write(1," ",1);
-	while (i-- && *str)
-		write(1, str++, 1);
-	while (j-- > 0 && flag->minus)
-		write(1," ",1);
-	return (cnt);
+        char    *num;
+        size_t  num_size;
+
+        num = int2hex(va_arg(ap, unsigned int), pin);
+        if (num == NULL)
+                return (0);
+        if (num[0] == '0' && flag->precision == 0)
+                num[0] = '\0';
+        num_size = ft_strlen(num);
+        if (flag->precision >= 0 && (size_t)flag->precision > num_size)
+        {
+                flag->precision = flag->precision - (int)num_size;
+                num_size = num_size + flag->precision;
+        }
+        else if (flag->zero == 1 && flag->width > num_size)
+        {
+                flag->precision = (int)(flag->width - num_size);
+                num_size = num_size + flag->precision;
+        }
+        else
+                flag->precision = 0;
+        print_flag(flag, num, num_size, 0);
+        free(num);
+        return ((int)flag->width + flag->indent);
 }
 
-int print_ptr(t_flag *flag, va_list ap)
+int                             print_ptr(t_flag *flag, va_list ap)
 {
-	int cnt;
-	int i;
-	char *str;
+        char    *num;
+        size_t  num_size;
 
-	cnt = 2;
-	str = int2hex(va_arg(ap, unsigned long long), &cnt);
-	i = 0;
-	if (flag->minus)
-	{
-		write(1, "0x", 2);
-		while (str[i])
-			write(1, &str[i++], 1);
-	}
-	i = flag->width - cnt - 1;
-	while (--i)
-	{
-		write(1, " ", 1);
-		cnt++;
-	}
-	i = 0;
-	if (!flag->minus)
-	{
-		write(1, "0x", 2);
-		while (str[i])
-			write(1, &str[i++], 1);
-	}
-	free(str);
-	return (cnt);
-}
-
-int print_int(t_flag *flag, va_list ap)
-{
-	int cnt;
-	int i;
-	int len;
-	char *str;
-	int sign;
-
-	i = 0;
-	str = int2str(va_arg(ap, int), &len);
-	sign = (str[i] == '-') ? 1 : 0;
-	cnt = 0;
-	if (flag->width > len && flag->width > flag->precision && !flag->zero && !flag->minus)
-		while (i++ < (cnt = flag->width - ((len <= flag->precision) ? flag->precision : len)))
-			write(1, " ",1);
-	i = 0;
-	write(1, &str[i], sign);
-	cnt += sign;
-	if (len <= (flag->zero ? flag->width : flag->precision))
-		while (i++ < (cnt = (flag->zero ? flag->width : flag->precision) - len) + sign)
-			write(1, "0", 1);
-	i = 0;
-	while (str[i + sign])
-	{
-		write(1, &str[sign + i++], 1);
-		cnt++;
-	}
-	i = 0;
-	if (flag->width > len && flag->width > flag->precision && !flag->zero && flag->minus)
-		while (i++ < (cnt = flag->width - ((len <= flag->precision) ? flag->precision : len)))
-			write(1, " ",1);
-	free(str);
-	return (cnt);
-}
-
-int print_uint(t_flag *flag, va_list ap)
-{
-	int cnt;
-	int i;
-	int len;
-	char *str;
-
-	i = 0;
-	str = int2str(va_arg(ap, unsigned int), &len);
-	cnt = 0;
-	if (flag->width > len && flag->width > flag->precision && !flag->zero &&!flag->minus)
-		while (i++ < (cnt = flag->width - ((len <= flag->precision) ? flag->precision : len)))
-			write(1, " ",1);
-	i = 0;
-	if (len <= (flag->zero ? flag->width : flag->precision))
-		while (i++ < (cnt = (flag->zero ? flag->width : flag->precision) - len))
-			write(1, "0", 1);
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i++], 1);
-		cnt++;
-	}
-	i = 0;
-	if (flag->width > len && flag->width > flag->precision && !flag->zero &&flag->minus)
-		while (i++ < (cnt = flag->width - ((len <= flag->precision) ? flag->precision : len)))
-			write(1, " ",1);
-	free(str);
-	return (cnt);
-}
-
-int print_hex(t_flag *flag, va_list ap, int pin)
-{
-	int cnt;
-	char *str;
-	int i;
-	int len;
-
-	str = int2hex(va_arg(ap, unsigned int), &len);
-	i = 0;
-	cnt = 0;
-	if (flag->width > len && flag->width > flag->precision && !flag->zero)
-		while (i++ < (cnt = flag->width - ((len <= flag->precision) ? flag->precision : len)))
-			write(1, " ",1);
-	i = 0;
-	if (len <= (flag->zero ? flag->width : flag->precision))
-		while (i++ < (cnt = (flag->zero ? flag->width : flag->precision) - len))
-			write(1, "0", 1);
-	i = 0;
-	while (str[i])
-	{	
-		str[i] = (pin == 1 && 'a' <= str[i] && str[i] <= 'z') ? str[i] - 32 : str[i];
-		write(1, &str[i++], 1);
-		cnt++;
-	}
-	free(str);
-	return (cnt);
-}
-
-int print_percent(t_flag *flag)
-{
-	int cnt;
-
-	cnt = 1;
-	write(1, "%", 1);
-	while (--(flag->width))
-	{
-		cnt++;
-		write(1, " ", 1);
-	}
-	return (cnt);
+        num = ptr2hex(va_arg(ap, unsigned long long));
+        if (num == NULL)
+                return (0);
+        if (num[2] == '0' && flag->precision == 0 && num[3] == '\0')
+                num[2] = '\0';
+        num_size = ft_strlen(num);
+        if (flag->precision >= 0 && (size_t)flag->precision + 2 > num_size)
+        {
+                flag->precision = flag->precision - (int)num_size + 2;
+                num_size = num_size + flag->precision;
+        }
+        else if (flag->zero == 1 && flag->width > num_size)
+        {
+                flag->precision = (int)(flag->width - num_size);
+                num_size = num_size + flag->precision;
+        }
+        else
+                flag->precision = 0;
+        print_flag(flag, num, num_size, 0);
+        free(num);
+        return ((int)flag->width + flag->indent);
 }
