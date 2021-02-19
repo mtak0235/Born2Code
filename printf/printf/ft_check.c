8d5d50979a2b3e6be650d43d33e9feb7efa127b4
mtak0235 <jesustark0235@gmail.com>
2021-02-19 15:01:20 +0100

    norm0

--- printf/printf/ft_check.c
+++ printf/printf/ft_check.c
@@ -1,48 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtak <mtak@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/17 16:05:04 by mtak              #+#    #+#             */
+/*   Updated: 2021/02/17 16:09:54 by mtak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 #include "ft_utils.h"
 #include "libft.h"
-int check_flag(t_flag *flag, char c)
+
+int	check_flag(t_flag *flag, char c)
 {
 	if (c == '-')
 		return (flag->minus = 1);
 	if (c == '0')
-    	return (flag->zero = 1);
+		return (flag->zero = 1);
 	if (c == ' ')
-    	return (flag->indent = 1);
+		return (flag->indent = 1);
 	return (0);
 }
 
-int check_precision(int c, va_list ap, t_flag *flag, int pin)
+int	check_precision(int c, va_list ap, t_flag *flag, int pin)
 {
 	if (ft_isdigit(c) && pin == 1)
 	{
-    	flag->precision = (flag->precision) * 10 + c - '0';
-    	return (1);
+		flag->precision = (flag->precision) * 10 + c - '0';
+		return (1);
 	}
 	else if (pin == 2)
 	{
-    	flag->precision = va_arg(ap, int);
+		flag->precision = va_arg(ap, int);
 		flag->star = 1;
-    	return (1);
-  	}
+		return (1);
+	}
 	return (0);
 }
 
-
-int check_width(int c, va_list ap, t_flag *flag, int pin)
+int	check_width(int c, va_list ap, t_flag *flag, int pin)
 {
 	if (ft_isdigit(c) && pin == 1)
 		return (flag->width = (flag->width) * 10 + c - '0');
 	else if (pin == 2)
 	{
-    	flag->width = va_arg(ap, int);
+		flag->width = va_arg(ap, int);
 		flag->star = 1;
-    	return (1);
+		return (1);
 	}
 	return (0);
 }
 
-int check_type(char c, t_flag *flag)
+int	check_type(char c, t_flag *flag)
 {
 	char	*set;
 	int		i;
--- printf/printf/ft_print.c
+++ printf/printf/ft_print.c
@@ -1,86 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtak <mtak@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/19 14:58:06 by mtak              #+#    #+#             */
+/*   Updated: 2021/02/19 14:58:11 by mtak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 #include "ft_utils.h"
 #include "libft.h"
 
 static void print_flag(t_flag *flag, char *num, int size, int ost)
 {
-        if (flag->indent)
-                ft_putchar_fd(' ', 1);
-        flag->width = size > flag->width ? size : flag->width;
-        if (flag->minus == 0)
-                while (size < flag->width)
-                {
-                        ft_putchar_fd(' ', 1);
-                        size++;
-                }
-        if (num[0] == '0' && num[1] == 'x')
-        {
-                ft_putstr_fd("0x", 1);
-                ost = 2;
-        }
-        while (flag->precision > 0 && flag->precision--)
-                ft_putchar_fd('0', 1);
-        ft_putstr_fd(num + ost, 1);
-        if (flag->minus == 1)
-                while (size < flag->width)
-                {
-                        size++;
-                        ft_putchar_fd(' ', 1);
-                }
+	if (flag->indent)
+		ft_putchar_fd(' ', 1);
+	flag->width = size > flag->width ? size : flag->width;
+	if (flag->minus == 0)
+		while (size++ < flag->width)
+			ft_putchar_fd(' ', 1);
+	if (num[0] == '0' && num[1] == 'x')
+	{
+		ft_putstr_fd("0x", 1);
+		ost = 2;
+	}
+	while (flag->precision > 0 && flag->precision--)
+		ft_putchar_fd('0', 1);
+	ft_putstr_fd(num + ost, 1);
+	if (flag->minus == 1)
+		while (size++ < flag->width)
+			ft_putchar_fd(' ', 1);
 }
 
-int                             print_hex(t_flag *flag, va_list ap, int pin)
+int			print_hex(t_flag *flag, va_list ap, int pin)
 {
-        char    *num;
-        int  num_size;
-
-        num = int2hex(va_arg(ap, unsigned int), pin);
-        if (num == NULL)
-                return (0);
-        if (num[0] == '0' && flag->precision == 0)
-                num[0] = '\0';
-        num_size = ft_strlen(num);
-        if (flag->precision >= 0 && flag->precision > num_size)
-        {
-                flag->precision = flag->precision - num_size;
-                num_size = num_size + flag->precision;
-        }
-        else if (flag->zero == 1 && flag->width > num_size)
-        {
-                flag->precision = flag->width - num_size;
-                num_size = num_size + flag->precision;
-        }
-        else
-                flag->precision = 0;
-        print_flag(flag, num, num_size, 0);
-        free(num);
-        return (flag->width + flag->indent);
+	char	*num;
+	int		num_size;
+	
+	num = int2hex(va_arg(ap, unsigned int), pin);
+	if (num == NULL)
+		return (0);
+	if (num[0] == '0' && flag->precision == 0)
+		num[0] = '\0';
+	num_size = ft_strlen(num);
+	if (flag->precision >= 0 && flag->precision > num_size)
+	{
+		flag->precision = flag->precision - num_size;
+		num_size = num_size + flag->precision;
+	}
+	else if (flag->zero == 1 && flag->width > num_size)
+	{	
+		flag->precision = flag->width - num_size;
+		num_size = num_size + flag->precision;
+	}
+	else
+		flag->precision = 0;
+	print_flag(flag, num, num_size, 0);
+	free(num);
+	return (flag->width + flag->indent);
 }
 
-int                             print_ptr(t_flag *flag, va_list ap)
+int			print_ptr(t_flag *flag, va_list ap)
 {
-        char    *num;
-        int  num_size;
+	char	*num;
+	int		num_size;
 
-        num = ptr2hex(va_arg(ap, unsigned long long));
-        if (num == NULL)
-                return (0);
-        if (num[2] == '0' && flag->precision == 0 && num[3] == '\0')
-                num[2] = '\0';
-        num_size = ft_strlen(num);
-        if (flag->precision >= 0 && flag->precision + 2 > num_size)
-        {
-                flag->precision = flag->precision - num_size + 2;
-                num_size = num_size + flag->precision;
-        }
-        else if (flag->zero == 1 && flag->width > num_size)
-        {
-                flag->precision = (flag->width - num_size);
-                num_size = num_size + flag->precision;
-        }
-        else
-                flag->precision = 0;
-        print_flag(flag, num, num_size, 0);
-        free(num);
-        return (flag->width + flag->indent);
+	num = ptr2hex(va_arg(ap, unsigned long long));
+	if (num == NULL)
+		return (0);
+	if (num[2] == '0' && flag->precision == 0 && num[3] == '\0')
+		num[2] = '\0';
+	num_size = ft_strlen(num);
+	if (flag->precision >= 0 && flag->precision + 2 > num_size)
+	{
+		flag->precision = flag->precision - num_size + 2;
+		num_size = num_size + flag->precision;
+	}
+	else if (flag->zero == 1 && flag->width > num_size)
+	{
+		flag->precision = (flag->width - num_size);
+		num_size = num_size + flag->precision;
+	}
+	else
+		flag->precision = 0;
+	print_flag(flag, num, num_size, 0);
+	free(num);
+	return (flag->width + flag->indent);
 }
