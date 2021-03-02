d5527f1046a1526c9d08d1ced35314d93a229ec2
mtak <jesustark0235@gmail.com>
2021-03-02 16:53:08 +0900

    save me

--- ft_server/srcs/run.sh
+++ ft_server/srcs/run.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
+#set authority
 chmod 775 /run.sh
-chown -R www-data:www-data /var/www/
 chmod -R 755 /var/www/
 
+#generate private key&certificate
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Tak/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
 mv localhost.dev.crt etc/ssl/certs/
 mv localhost.dev.key etc/ssl/private/
 chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
+
+#set nginx
 cp -rp /tmp/default /etc/nginx/sites-available/
 
+#set wordpress
 wget https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 mv wordpress/ var/www/html/
 chown -R www-data:www-data /var/www/html/wordpress
 cp -rp ./tmp/wp-config.php /var/www/html/wordpress
 
+#set mysql
 service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
 echo "CREATE USER IF NOT EXISTS 'mtak'@'localhost' IDENTIFIED BY 'mtak';" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'mtak'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 
+#set AutoIndex
+if [ "$AUTOINDEX" == "off" ];then
+	sed -i "s/autoindex on/#autoindex on/" /etc/nginx/sites-available/default
+fi
+
+#set phpMyAdmin
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
